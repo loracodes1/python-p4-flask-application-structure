e92a0749a4fa0d542c113ad5d64ed851bfcb7c3c
florence <loracodes3@gmail.com>
2025-01-14 12:07:37 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -4,8 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# append to server/app.py
-
 @app.route('/')
 def index():
     return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
